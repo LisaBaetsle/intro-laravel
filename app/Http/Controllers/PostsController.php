c0e01c94f2816a41a95920e478ea198a6bbb2392
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-10-13 16:55:20 +0200

    Link the stylesheets to the html

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,9 @@
+<?php
+
+class PostsController
+{
+  public function show()
+  {
+    return view('reviews');
+  }
+}
--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -2,4 +2,13 @@
 
 @section ('content')
 <h2>Let's write some reviews!</h2>
+
+<form action="/action_page.php">
+  <label for="name">Name:</label><br>
+  <input type="text" id="name" name="name" value="Jane"><br>
+  <label for="comment">Comment:</label><br>
+  <textarea name="comment" rows="10" cols="30"></textarea><br>
+  <input type="submit" value="Submit">
+</form>
+
 @endsection
